693407910b03b2b7d73f52d083887532332dffe5
Abdul Muneeb Swati <muneebswati00@gmail.com>
2023-12-27 09:43:12 +0500

    port changed

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const app  = express();
 
-app.get("/",(req,res)=>{
-    res.send("hello world")
+app.get("/api",(req,res)=>{
+    res.json("hello world");
 })
 
-app.listen(6000,()=>{
+app.listen(3000,()=>{
     console.log("server is running");
 })
